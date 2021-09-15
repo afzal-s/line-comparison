a0c8351817fac139ed9a77c5f58e7edd7f2c3602
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 15:17:49 +0530

    Checking Both Lines Are Equal, Greater Or Less.

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -8,17 +8,17 @@ public class LineComparison {
 		double distance1, distance2;
 		System.out.println("Welcome to Line Comparision Problem");
 		System.out.println("Enter the co-ordinates of first line");
-		System.out.println("Enter the values of first co-ordinates of Line1: ");
+		System.out.println("Enter the values of first co-ordinates of Line1:  ");
 		x1 = sc.nextInt();
 		y1 = sc.nextInt();
-		System.out.println("Enter the values of second co-ordinates of Line1: ");
+		System.out.println("Enter the values of second co-ordinates of Line1:  ");
 		x2 = sc.nextInt();
 		y2 = sc.nextInt();
 		System.out.println("Enter the co-ordinates of second line");
-		System.out.println("Enter the values of first co-ordinates of Line2: ");
+		System.out.println("Enter the values of first co-ordinates of Line2:  ");
 		x3 = sc.nextInt();
 		y3 = sc.nextInt();
-		System.out.println("Enter the values of second co-ordinates of Line2: ");
+		System.out.println("Enter the values of second co-ordinates of Line2:  ");
 		x4 = sc.nextInt();
 		y4 = sc.nextInt();
 		distance1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
@@ -27,9 +27,10 @@ public class LineComparison {
 		System.out.println("Length of the line2 : " + distance2);
 		if (distance1 == distance2) {
 			System.out.println("Both lines are equal.");
+		} else if (distance1 > distance2) {
+			System.out.println("Line1 is greater than Line2");
 		} else {
-			System.out.println("Both lines are not equal");
+			System.out.println("Line1 is smaller than Line2");
 		}
 	}
-
 }
